90f19a2120e7396bd085f4d7445e89d4ce2d3d1f
Jason <vivanaranja@gmail.com>
2017-12-07 21:27:00 -0800

    adding structure

---
+++ hangmanLetters.js
@@ -0,0 +1,9 @@
+var letter= function(character){
+    this.placeholder = '_';
+    this.letter =character;
+    this.letterGuessed = false;
+}
+
+
+
+module.exports = letter;
---
+++ hangmanWords.js
@@ -0,0 +1,5 @@
+var letter = require('./letter');
+
+var word = function(gameWord){
+
+}
