565401e7aa2aa830c2910af5e9227535ab1cf847
Jason <vivanaranja@gmail.com>
2017-12-10 21:44:34 -0800

    pseudocoding to flesh out game flow

--- nodeHangman.js
+++ nodeHangman.js
@@ -1,3 +1,10 @@
+// start game
+// choose a random word and display a '_' for each letter contained in the random word.
+// user chooses a letter and program compares the choice to the letters in the random word.
+// if a letter matches the user choice, then the '_' for that letter is replaced by the actual letter.
+// if the letter does not match then nothing changes; however the number of guesses left decriments by one.
+// gameplay continues until guesses left zeroes out or the full word is guessed.
+
 var inquirer = require('inquirer');
 var hangmanWords = require('./hangmanWords');
 var fs = require('fs');
@@ -12,6 +19,6 @@ var guessesLeft = 9;
 var gameWord = lexicon[Math.floor(Math.random() * lexicon.length)];
 //log random word
 console.log(gameWord);
-
+// log gameWord with '_' replacing gameWord letters.
 console.log(this.gameWord.wordDisplay());
 
